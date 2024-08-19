40de1af0659a98269f69616f34ee4b51467e792d
jblanc <blanchardjonah@gmail.com>
2024-08-18 19:39:47 -0700

    Title Change
    updated site title to redirect to check-in screen

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Button from './Button'
 import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 import "../CSSelements/Navbar.css"
 
-
 export default function Navbar(props) {
-
-
   const navigate = useNavigate()
 
   //list of navigation tabs
@@ -22,13 +21,10 @@ export default function Navbar(props) {
         onClick: "/login",
       },
     ]
-  
-
-  
 
   return (
       <nav className='nav--navbar'>
-          <h2 className='nav--title'>OASIS MEN'S GROUP</h2>
+          <Link><h2 className='nav--title' to="/">OASIS MEN'S GROUP</h2></Link>
           {/*render each of the tabs shown in the array tabData
           and update the state of each one when it is selected*/}
           {tabData.map((tab, index) => (
